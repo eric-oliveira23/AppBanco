10cf1d63e6c98fe11637311063d27ab5f4de884b
Eric <ericpoliveira23@gmail.com>
2022-03-10 14:59:38 -0300

    Adicionar arquivos de projeto.

---
+++ AppBanco/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppBanco
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void btn_login(object sender, EventArgs e)
+        {
+            try
+            {
+                string username = ent_User.Text;
+                int pass = int.Parse(ent_Pass.Text);             
+
+                if (ent_User.Text.Length <= 0 || ent_Pass.Text.Length < 4)
+                {
+                    await DisplayAlert("Alerta", "Dados inválidos.", "OK");
+                }
+                else
+                {
+                    await Navigation.PushAsync(new Page2(ent_User.Text));
+                }
+                
+            }
+
+            catch (Exception)
+            {
+               await DisplayAlert("Alerta", "Erro desconhecido.", "OK");
+            }
+           
+        }
+         
+        
+
+        }
+
+    }
+
---
+++ AppBanco/Page2.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBanco
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string user)
+        {
+            InitializeComponent();
+            lbl_username.Text = user;
+            lbl_username.FontSize = 25;
+            lblcard_user.Text = user;
+            lblcard_user.FontSize = 20;
+            lblSaldo.Text = saldo.ToString("C");
+            lblSaldo.FontSize = 20;
+        }
+
+        double saldo = 1220.89; 
+
+        private void btn_hide(object sender, EventArgs e)
+        {
+            if (lblSaldo.Text == "--------")
+            {
+                lblSaldo.Text = saldo.ToString("C");
+            }
+            else
+            {
+                lblSaldo.Text = "--------";
+            }     
+        }
+
+        private async void btn_back(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
